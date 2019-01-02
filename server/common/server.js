5422e8f7d0bbe4a8872ad159d5825534ae956fe3
Trent <tlarson@mediciventures.com>
2019-01-02 08:33:45 -0700

    initial, where the Swagger API works

---
+++ server/common/server.js
@@ -0,0 +1,32 @@
+import Express from 'express';
+import * as path from 'path';
+import * as bodyParser from 'body-parser';
+import * as http from 'http';
+import * as os from 'os';
+import cookieParser from 'cookie-parser';
+import swaggerify from './swagger';
+import l from './logger';
+
+const app = new Express();
+
+export default class ExpressServer {
+  constructor() {
+    const root = path.normalize(`${__dirname}/../..`);
+    app.set('appPath', `${root}client`);
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cookieParser(process.env.SESSION_SECRET));
+    app.use(Express.static(`${root}/public`));
+  }
+
+  router(routes) {
+    swaggerify(app, routes);
+    return this;
+  }
+
+  listen(port = process.env.PORT) {
+    const welcome = p => () => l.info(`up and running in ${process.env.NODE_ENV || 'development'} @: ${os.hostname()} on port: ${p}}`);
+    http.createServer(app).listen(port, welcome(port));
+    return app;
+  }
+}
---
+++ server/common/swagger/index.js
@@ -0,0 +1,51 @@
+import middleware from 'swagger-express-middleware';
+import * as path from 'path';
+
+export default function (app, routes) {
+  middleware(path.join(__dirname, 'Api.yaml'), app, (err, mw) => {
+    // Enable Express' case-sensitive and strict options
+    // (so "/entities", "/Entities", and "/Entities/" are all different)
+    app.enable('case sensitive routing');
+    app.enable('strict routing');
+
+    app.use(mw.metadata());
+    app.use(mw.files({
+      // Override the Express App's case-sensitive
+      // and strict-routing settings for the Files middleware.
+      caseSensitive: false,
+      strict: false,
+    }, {
+      useBasePath: true,
+      apiPath: process.env.SWAGGER_API_SPEC,
+      // Disable serving the "Api.yaml" file
+      // rawFilesPath: false
+    }));
+
+    app.use(mw.parseRequest({
+      // Configure the cookie parser to use secure cookies
+      cookie: {
+        secret: process.env.SESSION_SECRET,
+      },
+      // Don't allow JSON content over 100kb (default is 1mb)
+      json: {
+        limit: process.env.REQUEST_LIMIT,
+      },
+    }));
+
+    // These two middleware don't have any options (yet)
+    app.use(
+      mw.CORS(),
+      mw.validateRequest());
+
+    // Error handler to display the validation error as HTML
+    // eslint-disable-next-line no-unused-vars, no-shadow
+    app.use((err, req, res, next) => {
+      res.status(err.status || 500);
+      res.send(
+        `<h1>${err.status || 500} Error</h1>` +
+        `<pre>${err.message}</pre>`);
+    });
+
+    routes(app);
+  });
+}
