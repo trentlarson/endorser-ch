7577694ae6aea530215faccc8d73797607081ed2
Trent <trent@trentlarson.com>
2019-01-02 21:48:41 -0700

    switch to swagger-generator lib; add simple, incorrect JWT endpoints

--- server/api/controllers/examples/router.js
+++ server/api/controllers/examples/router.js
@@ -3,6 +3,17 @@ import controller from './controller';
 
 export default express
   .Router()
+/**
+ * This is isn't complete: see ../../../common/swagger/Api.yaml in the code history.
+ * @route POST /api/examples
+ */
   .post('/', controller.create)
+/**
+ * @route GET /api/examples
+ */
   .get('/', controller.all)
+/**
+ * @route GET /api/examples/{id}
+ * @param {string} id.path.required - the ID of the example to retrieve
+ */
   .get('/:id', controller.byId);
---
+++ server/api/services/jwt.service.js
@@ -0,0 +1,20 @@
+import l from '../../common/logger';
+import db from './jwt.db.service';
+
+class JwtService {
+  all() {
+    l.info(`${this.constructor.name}.all()`);
+    return db.all();
+  }
+
+  byId(id) {
+    l.info(`${this.constructor.name}.byId(${id})`);
+    return db.byId(id);
+  }
+
+  create(name) {
+    return db.insert(name);
+  }
+}
+
+export default new JwtService();
