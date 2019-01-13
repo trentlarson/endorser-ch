dea21bb30dda5ed68135479b8878a29360855b74
Trent <trent@trentlarson.com>
2019-01-13 06:52:06 -0700

    remove examples

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,5 @@
-import examplesRouter from './api/controllers/examples/router';
 import jwtRouter from './api/controllers/jwt/router';
 
 export default function routes(app) {
-  app.use('/api/examples', examplesRouter);
-
   app.use('/api/jwt', jwtRouter);
 }
