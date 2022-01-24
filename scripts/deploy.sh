067fd06b02bf25b58a987974d7726a14569740f3
Trent <trent@trentlarson.com>
2022-01-23 17:18:51 -0700

    enhance the deploy.sh script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+# This script is very similar to the one in https://github.com/trentlarson/uport-demo/blob/master/scripts/deploy.sh
+
 if [ -z "$1" ]
 then
-    echo "No release tag supplied (arg 1)"
+    echo 'No release environment supplied (arg 1 of "ubuntu" or "ubuntutest")'
     exit 1
 fi
 
 if [ -z "$2" ]
 then
-    echo "No SSH key file specified (arg 2)"
+    echo "No release tag supplied (arg 2)"
+    exit 1
+fi
+
+if [ -z "$3" ]
+then
+    echo "No SSH key file specified (arg 3)"
     exit 1
 fi
 
@@ -30,29 +38,46 @@ then
     echo "Continuing with deploy."
 fi
 
-git checkout $1
+git checkout $2
 
+if ! [ "$(echo $?)" -eq "0" ]
+then
+  echo ""
+  echo "You've got local changes that aborted switching to the branch, so will stop now."
+  exit 1
+fi
+
+USERNAME=$1
 DEPLOY_DIR=endorser-ch
 
-rsync -azv --exclude .git --exclude-from .gitignore -e "ssh -i $2" . ubuntu@endorser.ch:$DEPLOY_DIR
+rsync -azv --exclude .git --exclude-from .gitignore -e "ssh -i $3" . $USERNAME@endorser.ch:$DEPLOY_DIR
 
-ssh -i $2 ubuntu@endorser.ch << EOF
+ssh -i $3 $USERNAME@endorser.ch << EOF
+
+  echo "Logged in as:"
+  whoami
+
+  # I've brought the server to its knees trying to do this with the app still running.
+  echo "Killing any running processes. If it shows 'Usage' instructions then it wasn't running."
+  ps -u | grep "node dist/index.js" | grep -v "sh -c" | grep -v grep | awk "{print \$2}" | xargs kill
+
   cd $DEPLOY_DIR
 
-  echo "Running: npm ci"
+  echo "Running npm ci..."
   # Don't we need to add --production on the end of "npm ci"?
   npm ci
+  echo "... finished with npm ci"
 
-  echo "Running: npm compile"
-  npm run compile
+  # There's a "deploy-production" script we should look into.
+  #npm run deploy-production
 
   # I think this is useful, but it causes a failure at startup.
   #echo "Running: npm prune"
   #npm prune --production
 
-  perl -p -i -e "s/VERSION=.*/VERSION=$1/g" .env
+  perl -p -i -e "s/VERSION=.*/VERSION=$2/g" .env
 EOF
 
 git checkout master
 
-echo "Deployed.  Now log in and get into screen and start the app."
+echo "Deployed.  Now log in and start the app."
