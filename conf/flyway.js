c3069430d8f8d99a98e85297168e9da398be8932
Trent <trent@trentlarson.com>
2022-04-10 19:15:37 -0600

    upgrade flyway version & adjust

--- conf/flyway.js
+++ conf/flyway.js
@@ -9,10 +9,12 @@ const FILE_LOC = process.env.APP_DB_FILE || '../endorser-ch-' + process.env.NODE
 // fileLoc is my own addition since I'm using this same DB info inside my services
 module.exports = {
   fileLoc: FILE_LOC,
-  url: 'jdbc:sqlite:' + FILE_LOC,
-  schemas: 'main',
-  locations: 'filesystem:./sql',
-  sqlMigrationSuffix: '.sqlite3',
-  user: process.env.DBUSER,
-  password: process.env.DBPASS
+  flywayArgs: {
+    url: 'jdbc:sqlite:' + FILE_LOC,
+    schemas: 'main',
+    locations: 'filesystem:./sql',
+    sqlMigrationSuffixes: '.sqlite3',
+    user: process.env.DBUSER,
+    password: process.env.DBPASS
+  }
 }
